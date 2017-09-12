4e01674e7c1e0dd9e7d7cf8be96fa2a57958fe79
Ricardo Navares <30971856+rnavares@users.noreply.github.com>
2017-09-12 09:11:35 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,16 +3,18 @@
 #include "GA.hpp"
 
 int main() {
-
+    // read the problem and the parameters
     std::vector< std::vector<int>> m = read_file("Sudoku_Sample.txt");
     inputs in = read_parameters("Parameters.txt");
 
     in.print();
 
+    // request user inputs
     int x;
     std::cout << "\n Parameter test (1) success rate (2) solve (3): ";
     std::cin  >> x;
-
+    
+    // parameter test
     if (x == 1) {
         std::ofstream F;
         F.open("output.csv");
@@ -21,15 +23,15 @@ int main() {
         F << "Prob Mutation " << in.prob_mut <<"\n";
         F << "Prob CO " << in.prob_co <<"\n";
         std::vector<std::vector<int>> results;
+
         for (int i = 0; i <15; i++) {
             GA gen_algo(m,in);
             gen_algo.init();
             gen_algo.evolve();
-
             results.push_back(gen_algo.get_bestFit());
-
         }
-
+    
+        // show results
         for (int k = 0 ; k < (int) results.size();k++) {
                 for(int l = 0 ; l < (int) results[k].size(); l++) {
                     F << results[k][l] << ",";
@@ -40,7 +42,8 @@ int main() {
         F.close();
 
     }
-
+    
+    // run a number of test to chekc the success rate
     if (x==2) {
         for (int i= 0; i <10; i++) {
             GA gen_algo(m,in);
@@ -53,7 +56,6 @@ int main() {
         GA gen_algo(m,in);
         gen_algo.init();
         gen_algo.evolve();
-
     }
 
     return 0;
